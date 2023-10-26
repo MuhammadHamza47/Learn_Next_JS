dc76b604a174b996b92ca81c20722d4b8ba6dcdb
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-10-26 16:55:42 +0500

    contact page update

--- html-to-next/app/contact/page.js
+++ html-to-next/app/contact/page.js
@@ -6,7 +6,12 @@ import Button from "../(components)/button/button";
 export default function Contact() {
   return (
     <>
+
+      {/* Header Section */}
       <Header title='Contact Us' />
+
+      {/* Location Section */}
+
       <section className="loacation">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d111989.31313237658!2d77.32498792347556!3d28.699635117457326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf1bb41c50fdf%3A0xe6f06fd26a7798ba!2sGhaziabad%2C%20Uttar%20Pradesh!5e0!3m2!1sen!2sin!4v1630495771496!5m2!1sen!2sin"
@@ -17,6 +22,8 @@ export default function Contact() {
           loading="lazy"
         ></iframe>
       </section>
+
+      {/* Contact Us Section */}
 
       <section className="contact-us">
         <div className="row">
