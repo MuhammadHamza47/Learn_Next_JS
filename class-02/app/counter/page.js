29b59b9fe285a995c4dcc406ca79c1376142e4de
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-10-27 23:17:10 +0500

    counter page

---
+++ class-02/app/counter/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Count from '../(components)/count/count'
+
+export default function Counter() {
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <Count/>
+      
+    </div>
+  )
+}
