f3597b054d240947023fed441391da90e34fad90
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-10-28 22:12:37 +0500

    class-05 update

--- class-05/app/page.js
+++ class-05/app/page.js
@@ -1,41 +1,101 @@
+'use client'
+import { useState } from "react";
 
-var islogdIn = true;
-let userScore = 40;
-let grade = '';
-if (userScore >= 90 && userScore <= 100) {
-  grade = 'A';
-  
-}else if (userScore >= 80 && userScore <= 89) {
-  grade = 'B';
-}else if (userScore >= 70 && userScore <= 79) {
-  grade = 'C';
-}else if (userScore >= 60 && userScore <= 69) {
-  grade = 'D';
-}else{
-  grade = 'F';
-}
 export default function Home() {
-  return (
-   <div>
-   <h1>Welcome to Next Js 14</h1>
-   {
-    userScore >= 60 ? 
-    <h2>User is passed and grade is {grade}</h2>:
-    <h2>User is failed and grade is {grade}</h2>
-   }
+  const userNames = ["Hamza", "Huzaifa", "Saad", "Abbas", "Ahsan", "Hassan"];
+  const [students, setStudents]= useState( [
+    {
+      name: "Hamza",
+      rollNo: 21,
+      section: "A",
+    },
+    {
+      name: "Huzaifa",
+      rollNo: 22,
+      section: "C",
+    },
+    {
+      name: "Saad",
+      rollNo: 23,
+      section: "D",
+    },
+    {
+      name: "Abbas",
+      rollNo: 42,
+      section: "B",
+    },
+    {
+      name: "Ahsan",
+      rollNo: 34,
+      section: "D",
+    },
+    {
+      name: "Hassan",
+      rollNo: 26,
+      section: "E",
+    },
+  ]);
 
-   {islogdIn && <h1>Muhammad Hamza</h1>}
-     {islogdIn?(<button>
-    logout
-    </button>)
-   :
-   (<button>
-    login
-    </button>)
-   
+  const deleteHandler = (e) => {
+let restStudent = students.filter((item)=>{
+  if(item.rollNo !== e){
+    return item 
+
+  }
+})
+
+    setStudents(restStudent)
+
   }
 
-   </div>
-   
-  )
+  return (
+    <>
+{/*     
+      {userNames.map((item, i) => {
+        return (
+          <div key={i}>
+            <h1>Hello! Welcome to {item} </h1>
+            <hr />
+          </div>
+        );
+      })
+      }
+ */}
+
+{/*       
+      {students.map((item, i) => {
+        return (
+          <div key={i}>
+            <h1>Student Name: {item.name} </h1>
+            <h2>Roll No: {item.rollNo} </h2>
+            <h3>Section: {item.section}</h3>
+            <hr />
+          </div>
+        );
+      })
+      } */}
+
+
+      <table >
+        <tr>
+          <th>Name</th>
+          <th>Roll No</th>
+          <th>Section</th>
+          <th>Action</th>
+        </tr>
+        {
+          students.map((item, i) => {
+            return (
+              <tr key={i}>
+                <td>{item.name}</td>
+                <td>{item.rollNo}</td>
+                <td>{item.section}</td>
+                <td><button onClick={()=>deleteHandler(item.rollNo)}>Delete</button></td>
+              </tr>
+            );
+          })
+        }
+      </table>
+    </>
+  );
 }
