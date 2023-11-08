a55210b2b64800b3483f4066052245e13be049a5
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-08 20:07:43 +0500

    page.js file

--- contentful-cms/app/page.js
+++ contentful-cms/app/page.js
@@ -1,41 +1,37 @@
+import client from "@/lib/contentfulClient"
 
-var islogdIn = true;
-let userScore = 40;
-let grade = '';
-if (userScore >= 90 && userScore <= 100) {
-  grade = 'A';
-  
-}else if (userScore >= 80 && userScore <= 89) {
-  grade = 'B';
-}else if (userScore >= 70 && userScore <= 79) {
-  grade = 'C';
-}else if (userScore >= 60 && userScore <= 69) {
-  grade = 'D';
-}else{
-  grade = 'F';
+const fetchServices = async () => {
+  let responce = await client.getEntries({content_type: "services"})
+ 
+  const services = responce.items.map((item) => {
+    return{
+      title: item.fields.title,
+      description: item.fields.description,
+      image: item.fields.image.fields.file.url,
+    }
+  })
+  return services
 }
-export default function Home() {
+export default async function Home() {
+  const services = await fetchServices()
+  console.log("fetchServices",services);
   return (
    <div>
-   <h1>Welcome to Next Js 14</h1>
+   <h1>Home Services</h1>
+
    {
-    userScore >= 60 ? 
-    <h2>User is passed and grade is {grade}</h2>:
-    <h2>User is failed and grade is {grade}</h2>
+    services.map((service, i) => {
+      return(
+        <div key={i}>
+          <h3>{service.title}</h3>
+          <p>{service.description}</p>
+          <img src={service.image} style={{width:"200px"}} />
+          <hr />
+        </div>
+      )
+    })
    }
-
-   {islogdIn && <h1>Muhammad Hamza</h1>}
-     {islogdIn?(<button>
-    logout
-    </button>)
-   :
-   (<button>
-    login
-    </button>)
-   
-  }
-
    </div>
-   
+
   )
 }
