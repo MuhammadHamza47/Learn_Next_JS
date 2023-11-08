2428f5cce50455108692465f5c1a91470778afac
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-08 21:39:59 +0500

    commit

--- sanityCMS/lib/contentfulClient.js
+++
@@ -1,9 +0,0 @@
-var contentful = require('contentful')
-const { space } = require('postcss/lib/list')
-
-const client = contentful.createClient({
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-    space: process.env.CONTENTFUL_SPACE_ID,
-})
-
-export default client;
