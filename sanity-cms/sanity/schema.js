e7eeeb5166c74aeec863bb5a8d073de567204d65
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-09 20:43:50 +0500

    sanity

--- sanity-cms/sanity/schema.js
+++ sanity-cms/sanity/schema.js
@@ -1,7 +1,7 @@
 // import {blockContent} from './schemas/blockContent'
 // import {category} from './schemas/category'
 // import {post} from './schemas/post'
-// import {author} from './schemas/author'
+// import {author} from './schemas/author'//
 import {services} from './schemas/services'
 
 export const schema = {
