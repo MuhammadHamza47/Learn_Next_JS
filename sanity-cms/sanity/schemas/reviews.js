aba87ca0cea63564955186b4c7c9174e141e8345
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-10 17:03:17 +0500

    reviews

---
+++ sanity-cms/sanity/schemas/reviews.js
@@ -0,0 +1,31 @@
+export const reviews = {
+    name: 'reviews',
+    title: 'Reviews',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'position',
+        title: 'Position',
+        type: 'string',
+      },
+      {
+        name: 'description',
+        title: 'Description',
+        type: 'string',
+      },
+      
+    ],
+    preview: {
+      select: {
+        name: 'name',
+        position: 'position',
+        description: 'description',
+      },
+    },
+  }
+  
