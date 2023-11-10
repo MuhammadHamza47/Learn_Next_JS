3d4a55e8a319e4277d3acb92f188014227b6f0a5
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-10 17:04:27 +0500

    sanity frontenf

--- sanity-cms/app/counter/page.js
+++ sanity-cms/app/counter/page.js
@@ -1,11 +1,39 @@
-import React from 'react'
-import Count from '../(components)/count/count'
+import { client } from "@/sanity/lib/client"
 
-export default function Counter() {
+const fetchServices = async () => {
+  const services = await client.fetch(
+    '*[_type == "services"]',
+    {},
+    {
+      cache: 'no-cache',
+    }
+    
+    );
+  console.log('Fetching services', services);
+  return services;
+}
+
+
+ async function  Counter() {
+ const services = await fetchServices();
   return (
     <div>
-      <h1>Counter App</h1>
-      <Count/>
+    <h1>Data from Sanity</h1>
+   {
+    services.map((service)=>{
+      return (
+        <>
+        <h2>{service.title}</h2>
+        <p>{service.description}</p>
+        <p>{service._updatedAt}</p>
+        {/* <img src={service.mainImage.asset._ref} width={200} alt={service.mainImage.alt} /> */}
+        <hr />
+        </>
+      )
+    })
+   }
+      
     </div>
   )
 }
+export default Counter
