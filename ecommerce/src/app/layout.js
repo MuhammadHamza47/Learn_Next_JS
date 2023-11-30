8e085ab0080db89435ad45c7acaa2b5fc962c3c7
Muhammad Hamza <hafizhamza2247@gmail.com>
2023-11-30 22:24:25 +0500

    layout

--- ecommerce/src/app/layout.js
+++ ecommerce/src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import GlobalState from "@/context";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GlobalState>
+          <Navbar/>
+          <main>
+            {children}
+            </main>
+        </GlobalState>
+      </body>
     </html>
   );
 }
